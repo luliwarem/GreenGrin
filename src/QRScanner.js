6ff9c9cbb4532ce3de6aab7f784b7a53639eaff3
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-04 11:42:54 -0300

    funciona pero notework error

--- src/QRScanner.js
+++ src/QRScanner.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { CurrentRenderContext } from "@react-navigation/native";
-import { axios } from "axios";
+import axios from "axios";
 
 // npm install react-native-camera react-native-qrcode-scanner
 // import { RNCamera } from 'react-native-camera'
@@ -19,23 +19,103 @@ export default function QRScanner({ navigation }) {
   const [qrscan, setQrscan] = useState("No result");
   const [scanResults, setScanResults] = useState([]);
 
+  const fetchData2 = (puntos) => {
+    axios
+      .put("https://10.152.2.134:3000/user/1", { puntos: puntos })
+      .then((response) => {
+        // Maneja la respuesta de la API
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Maneja el error en caso de fallo de la solicitud
+        console.error(error);
+      });
+  };
+
+  const fetchData = (
+    idEstacion,
+    idUsuario,
+    botellasIngresadas,
+    date,
+    puntos
+  ) => {
+    console.log("Llamando a movimientos");
+    axios
+      .post("http://10.152.2.134:3000/movimientos/", {
+        Id_Estaciones: idEstacion,
+        Id_Usuario: idUsuario,
+        Fecha: date,
+        CantBotellas: botellasIngresadas,
+        Puntos: puntos,
+      })
+      .then((response) => {
+        console.log("rta de movimientos: ");
+        // Maneja la respuesta de la API
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Maneja el error en caso de fallo de la solicitud
+        console.error(error);
+      });
+  };
+
+  const onQRCodeRead = (e) => {
+    // para manejar la informacion del codigo QR leido
+    //console.log(e.data);
+    console.log(
+      [idEstacion, idUsuario, botellasIngresadas, date, puntos].join(" - ")
+    );
+    fetchData(idEstacion, idUsuario, botellasIngresadas, date, puntos);
+    fetchData2(puntos);
+    //usar el set de idestacion y el de botellas para setearlo para que se pueda postear en movimientos, y calcular puntos para updatear en usuario y en mov.
+    //sacar la fecha y hora de hoy y setearlo en date, cuando alguien escanea el qr. tambien lo posteo en mov
+    //ademas posteo en mov
+  };
+
   const askForCameraPermission = () => {
     (async () => {
-      const status = await BarCodeScanner.requestPermissionsAsync()
-        .then((res) => console.log(res))
-        .catch((error) => console.log());
-      setHasPermission(status === "granted");
+      //const status = await
     })();
   };
 
   useEffect(() => {
-    askForCameraPermission();
+    BarCodeScanner.requestPermissionsAsync()
+      .then((res) => {
+        console.log(res);
+        setHasPermission(res.granted === "granted");
+      })
+      .catch((error) => console.log());
+
+    //askForCameraPermission();
   }, []);
+
+  useEffect(() => {
+    console.log(
+      [idEstacion, idUsuario, botellasIngresadas, date, puntos].join(" - ")
+    );
+    onQRCodeRead();
+  }, [idEstacion, idUsuario, botellasIngresadas, date, puntos]);
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     setText(data);
+
     console.log("Type: " + type + "\nData: " + data);
+
+    console.log("TIPO!!!!", typeof data);
+
+    const obj = JSON.parse(data);
+
+    console.log("OBJ!!!!", obj);
+    setBotellasIngresadas(obj.botellas);
+    setIdEstacion(obj.idEstacion); // Maneja la respuesta de la API
+    setDate(Date.now());
+    setPuntos(obj.botellas * 100);
+    setIdUsuario(1);
+  };
+
+  const handleError = (err) => {
+    console.error(err);
   };
 
   if (hasPermission === null) {
@@ -48,79 +128,18 @@ export default function QRScanner({ navigation }) {
         />
       </View>
     );
+  } else {
+    return (
+      <View style={styles.container}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+        <Text style={styles.escanea}>Escanea el codigo QR</Text>
+      </View>
+    );
   }
-
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = axios
-        .post("https://10.152.2.134:3000/movimientos/", {
-          idEstacion: idEstacion,
-          idUsuario: idUsuario,
-          botellasIngresadas: botellasIngresadas,
-          fecha: date,
-          puntos: puntos,
-        })
-        .then((response) => {
-          // Maneja la respuesta de la API
-          console.log(response.data);
-        })
-        .catch((error) => {
-          // Maneja el error en caso de fallo de la solicitud
-          console.error(error);
-        });
-    }
-     // ejecutamos la función de búsqueda de datos
-  }, []);
-
-
-  useEffect(() => {
-    async function fetchData2() {
-      const response = axios
-        .put("https://10.152.2.134:3000/user/1", { puntos: puntos })
-        .then((response) => {
-          // Maneja la respuesta de la API
-          console.log(response.data);
-        })
-        .catch((error) => {
-          // Maneja el error en caso de fallo de la solicitud
-          console.error(error);
-        });
-    }
-     // ejecutamos la función de búsqueda de datos
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      <Text style={styles.escanea}>Escanea el codigo QR</Text>
-    </View>
-  );
 }
-
-onQRCodeRead = (e) => {
-  // para manejar la informacion del codigo QR leido
-  console.log(e.data);
-  console.log(e.data.botellas);
-  fetchData();
-  fetchData2();
-  //usar el set de idestacion y el de botellas para setearlo para que se pueda postear en movimientos, y calcular puntos para updatear en usuario y en mov.
-  //sacar la fecha y hora de hoy y setearlo en date, cuando alguien escanea el qr. tambien lo posteo en mov
-  //ademas posteo en mov
-};
-
-const handleScan = (data) => {
-  if (data) {
-    setScanResults([...scanResults, data]);
-  }
-};
-
-const handleError = (err) => {
-  console.error(err);
-};
 
 const styles = StyleSheet.create({
   container: {
