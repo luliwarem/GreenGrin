be89b8a4ffbfac2a8d4a5e002d2a60c3ca0ad25e
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-02 12:09:11 -0300

    no se ven las fotos en cel

--- src/Historial.js
+++ src/Historial.js
@@ -1,20 +1,18 @@
-import React, {useState} from "react"
-import {Text, View, StyleSheet, FlatList} from "react-native"
+import React, { useState } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 export default function Historial(navigation) {
+  const [historial, setHistorial] = useState([]);
 
-    const [historial, setHistorial] = useState([])
-
-
-    useEffect(() => {
-        async function getByUserId(userId) {
-          const response = await axios.get(
-            `http://localhost:3000/movimientos/user/${id}`
-          );
-          setHistorial(response.data);
-        }
-        getByUserId(id);
-    }, []);
+  useEffect(() => {
+    async function getByUserId(userId) {
+      const response = await axios.get(
+        `http://localhost:3000/movimientos/user/${id}`
+      );
+      setHistorial(response.data);
+    }
+    getByUserId(id);
+  }, []);
 
   const Item = ({ fecha, cantBotellas, puntos }) => (
     <View style={styles.item}>
@@ -24,26 +22,27 @@ export default function Historial(navigation) {
     </View>
   );
 
-    return(
-        <View style={styles.container}>
-            
-           <FlatList
-           data={historial}
-           renderItem={({ item }) => (
-             <Item fecha={item.fecha} cantBotellas={item.cantBotellas} puntos={item.puntos} />
-           )}
-           />
-
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={historial}
+        renderItem={({ item }) => (
+          <Item
+            fecha={item.fecha}
+            cantBotellas={item.cantBotellas}
+            puntos={item.puntos}
+          />
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "white",
-    },
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+});
